fc2d9bb76970d8d0dd380db1b90d7a32d3571c4c
Rafael Perez <rafaelandresph@gmail.com>
2021-09-20 20:32:48 -0400

    Add project files.

---
+++ StudioOne/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioOne {
+    class Circle {
+
+        private double radius;
+        public Circle(double Radius) {
+
+
+            radius = Radius;
+
+
+        }
+
+        public double getArea() {
+
+            return Math.PI * radius * radius;
+
+        }
+
+        public double getCircumference() {
+
+            return 2 * Math.PI * radius;
+
+        }
+
+        public double getDiameter() {
+
+            return 2 * radius;
+
+        }
+
+    }
+}
---
+++ StudioOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace StudioOne {
+    class Program {
+        static void Main(string[] args) {
+
+            while(true) {
+
+                Console.WriteLine("Enter your radius: ");
+
+                string input = Console.ReadLine();
+                if(!double.TryParse(input, out double result)) {
+                    // .. error with input
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine(input.GetType());
+
+                if(double.Parse(input) > 0) {
+
+
+
+                    Circle circle = new Circle(double.Parse(input));
+
+                    Console.WriteLine("How many miles per gallon in your vehicle? ");
+                    input = Console.ReadLine();
+                    double milesPerGallon = double.Parse(input);
+                    double results = circle.getCircumference() / milesPerGallon;
+
+                    Console.WriteLine($"Area of circle: {circle.getArea()}\nCircumference of circle: {circle.getCircumference()}\nDiameter of circle: {circle.getDiameter()}" +
+                            $"\nIt takes {results} gallon of gas to go around the circle");
+
+                }
+                else {
+
+                    Console.WriteLine("Please enter number greater than zero. :)");
+
+                }
+            }
+        }
+    }
+}
